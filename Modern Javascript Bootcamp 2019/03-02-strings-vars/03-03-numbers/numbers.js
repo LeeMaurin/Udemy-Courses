02066f42ce01ba52a26de29458e0a0c3b2019791
Lee Maurin <leem4421@gmail.com>
2019-03-14 21:39:22 -0500

    Delete numbers.js

--- Modern Javascript Bootcamp 2019/03-02-strings-vars/03-03-numbers/numbers.js
+++
@@ -1,8 +0,0 @@
-// let num = 5.5;
-// console.log(num);
-
-let studentScore = 18;
-let maxScore = 20;
-let percent = 'Your score is ' + (studentScore / maxScore) * 100 + '%';
-
-console.log(percent);
