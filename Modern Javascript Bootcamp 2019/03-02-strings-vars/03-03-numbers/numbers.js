75326dde1297a3876f9fc14090c66bb798182986
Lee Maurin <leem4421@gmail.com>
2019-03-14 21:42:38 -0500

    Add files via upload

---
+++ Modern Javascript Bootcamp 2019/03-02-strings-vars/03-03-numbers/numbers.js
@@ -0,0 +1,8 @@
+// let num = 5.5;
+// console.log(num);
+
+let studentScore = 18;
+let maxScore = 20;
+let percent = 'Your score is ' + (studentScore / maxScore) * 100 + '%';
+
+console.log(percent);
