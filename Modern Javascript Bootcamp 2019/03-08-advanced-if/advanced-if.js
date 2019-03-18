3e4200d4d07f1214b34b4f36a6306fc742ad47e3
Lee Maurin <leem4421@gmail.com>
2019-03-18 14:31:04 -0500

    Add files via upload

---
+++ Modern Javascript Bootcamp 2019/03-08-advanced-if/advanced-if.js
@@ -0,0 +1,9 @@
+let temp = 20;
+
+if (temp <= 32) {
+  console.log('It is too cold outside!');
+} else if (temp >= 110) {
+  console.log('It is too hot outside!');
+} else {
+  console.log('It is pretty nice outside!');
+}
