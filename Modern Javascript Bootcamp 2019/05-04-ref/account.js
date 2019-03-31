17f2b6a5ffa23af7698a17e05faa97c824ade9af
Lee Maurin <leem4421@gmail.com>
2019-03-31 17:40:13 -0500

    Add files via upload

---
+++ Modern Javascript Bootcamp 2019/05-04-ref/account.js
@@ -0,0 +1,36 @@
+let myAccount = {
+  name: 'Lee',
+  expenses: 0,
+  income: 0
+};
+
+let addIncome = function(account, amount) {
+  account.income = account.income + amount;
+  // console.log(`amount:${amount} myAccount.income ${account.income} `);
+};
+
+let addExpense = function(account, amount) {
+  account.expenses = account.expenses + amount;
+  // console.log(`amount:${amount} myAccount.expense ${account.expenses} `);
+};
+
+let resetAccount = function(account) {
+  account.income = 0;
+  account.expenses = 0;
+};
+
+let getAccountSummary = function(account) {
+  console.log(
+    `Account for ${account.name} has $${account.income - account.expenses}. $${
+      account.expenses
+    } for expenses. $${account.income} for income.`
+  );
+};
+// Account for Lee has $900.  $100 for expenses. $900 for income.
+
+addIncome(myAccount, 100);
+getAccountSummary(myAccount);
+addExpense(myAccount, 500);
+getAccountSummary(myAccount);
+resetAccount(myAccount);
+getAccountSummary(myAccount);
