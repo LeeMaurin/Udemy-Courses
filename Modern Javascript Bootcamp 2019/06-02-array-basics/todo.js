38940bdc77a33eb9c3db202e0da2459a8a603c0b
Lee Maurin <leem4421@gmail.com>
2019-04-05 22:38:10 -0500

    Add files via upload

---
+++ Modern Javascript Bootcamp 2019/06-02-array-basics/todo.js
@@ -0,0 +1,4 @@
+const toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
+console.log(`You have ${toDoList.length} todos.`);
+console.log(`ToDos: ${toDoList[0]}`);
+console.log(`ToDos: ${toDoList[toDoList.length - 2]}`);
