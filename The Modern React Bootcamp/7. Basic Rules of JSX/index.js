b348fbde3604a30b27dc2199e7aa50b6d32403ba
Lee Maurin <leem4421@gmail.com>
2019-04-28 11:49:08 -0500

    Update index.js

--- The Modern React Bootcamp/7. Basic Rules of JSX/index.js
+++ The Modern React Bootcamp/7. Basic Rules of JSX/index.js
@@ -1,8 +1,10 @@
+// A basic class component that shows a picture
+
 class Hello extends React.Component {
   render() {
     return (
       <div>
-        <h1>Da picture</h1>
+        <h1>Scooter Picture</h1>
         <img
           src="https://images.unsplash.com/45/aXpQLs4AQ5e1EtmeYkz3_20140524_124026.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
           alt="Building with scooter"
