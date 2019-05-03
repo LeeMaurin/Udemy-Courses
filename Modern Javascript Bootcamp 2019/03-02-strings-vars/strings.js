d065fde03bf7198fa41deb7bde2c1bfa222f914f
Lee Maurin <leem4421@gmail.com>
2019-05-03 10:38:30 -0500

    Update strings.js

--- Modern Javascript Bootcamp 2019/03-02-strings-vars/strings.js
+++ Modern Javascript Bootcamp 2019/03-02-strings-vars/strings.js
@@ -1,3 +1,8 @@
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node strings.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
+
+
 let name = 'Lee Maurin';
 
 console.log(name);
