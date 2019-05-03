0bb8cf1083afda6c4550bb730071c0b395740398
Lee Maurin <leem4421@gmail.com>
2019-05-03 10:45:54 -0500

    Update convert.js

--- Modern Javascript Bootcamp 2019/03-05-temp-convert/convert.js
+++ Modern Javascript Bootcamp 2019/03-05-temp-convert/convert.js
@@ -1,3 +1,7 @@
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node convert.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
+
 let farenheit = 32;
 let celsius = (farenheit - 32) * (5 / 9);
 let kelvin = (farenheit + 459.67) * (5 / 9);
