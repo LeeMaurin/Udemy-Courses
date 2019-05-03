9051d32da3857d55f5ddf4fdae900a72659df06e
Lee Maurin <leem4421@gmail.com>
2019-05-03 10:51:25 -0500

    Update multiple-args.js

--- Modern Javascript Bootcamp 2019/04-04-multiple-args/multiple-args.js
+++ Modern Javascript Bootcamp 2019/04-04-multiple-args/multiple-args.js
@@ -1,3 +1,7 @@
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node multiple-args.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
+
 let tip = function(total, tip = 0.2) {
   let totalWithTip = total + total * tip;
   console.log('Your total with tip is: $' + totalWithTip);
