e2de2915bbcf6e86e06a3f8bcaf2a3bd96945bf7
Lee Maurin <leem4421@gmail.com>
2019-05-03 10:49:25 -0500

    Update and-or.js

--- Modern Javascript Bootcamp 2019/03-09-and-or/and-or.js
+++ Modern Javascript Bootcamp 2019/03-09-and-or/and-or.js
@@ -1,3 +1,7 @@
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node and-or.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
+
 let isGuest1Vegan = true;
 let isGuest2Vegan = true;
 
