3a21bf7d841badc44a057a696ef9ad711a5bd2b2
Lee Maurin <leem4421@gmail.com>
2019-05-03 10:43:59 -0500

    Update numbers.js

--- Modern Javascript Bootcamp 2019/03-03-numbers/numbers.js
+++ Modern Javascript Bootcamp 2019/03-03-numbers/numbers.js
@@ -1,5 +1,6 @@
-// let num = 5.5;
-// console.log(num);
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node numbers.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
 
 let studentScore = 18;
 let maxScore = 20;
