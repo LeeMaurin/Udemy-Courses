968723240ee47911caed705e7c72385814b8457e
Lee Maurin <leem4421@gmail.com>
2019-05-03 10:39:08 -0500

    Update stringsc.js

--- Modern Javascript Bootcamp 2019/03-02-strings-vars/stringsc.js
+++ Modern Javascript Bootcamp 2019/03-02-strings-vars/stringsc.js
@@ -1,3 +1,8 @@
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node stringsc.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
+
+
 let city = 'Cedar Park';
 let country = 'United States';
 let location = city + ', ' + country;
