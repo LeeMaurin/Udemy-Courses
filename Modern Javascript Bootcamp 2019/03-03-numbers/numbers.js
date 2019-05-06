06b92d33bf0c7fbf382418da765ba85bed150007
Lee Maurin <leem4421@gmail.com>
2019-05-06 18:22:12 -0500

    Update numbers.js

--- Modern Javascript Bootcamp 2019/03-03-numbers/numbers.js
+++ Modern Javascript Bootcamp 2019/03-03-numbers/numbers.js
@@ -2,6 +2,8 @@
 // At the terminal prompt, type: node numbers.js. 
 // Note: you must be in the subdirectory where the JavaScript file is located.
 
+// This code generates a percent grade score. 
+
 let studentScore = 18;
 let maxScore = 20;
 let percent = 'Your score is ' + (studentScore / maxScore) * 100 + '%';
