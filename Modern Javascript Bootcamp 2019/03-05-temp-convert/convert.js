382b121de9a708bef51230307ca70a3ca6cf3423
Lee Maurin <leem4421@gmail.com>
2019-05-06 18:24:44 -0500

    Update convert.js

--- Modern Javascript Bootcamp 2019/03-05-temp-convert/convert.js
+++ Modern Javascript Bootcamp 2019/03-05-temp-convert/convert.js
@@ -2,6 +2,8 @@
 // At the terminal prompt, type: node convert.js. 
 // Note: you must be in the subdirectory where the JavaScript file is located.
 
+// This code converts 32 degrees in farenheit to it's celsius and kelvin equilavent.
+
 let farenheit = 32;
 let celsius = (farenheit - 32) * (5 / 9);
 let kelvin = (farenheit + 459.67) * (5 / 9);
