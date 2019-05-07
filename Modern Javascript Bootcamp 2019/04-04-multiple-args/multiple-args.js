59270018f2badf4d16bc9e4b88955bf8268c8f7a
Lee Maurin <leem4421@gmail.com>
2019-05-06 19:02:37 -0500

    Update multiple-args.js

--- Modern Javascript Bootcamp 2019/04-04-multiple-args/multiple-args.js
+++ Modern Javascript Bootcamp 2019/04-04-multiple-args/multiple-args.js
@@ -2,6 +2,8 @@
 // At the terminal prompt, type: node multiple-args.js. 
 // Note: you must be in the subdirectory where the JavaScript file is located.
 
+// This code utilizes a function, with a default attribute, to calculate meal check total with tip.
+
 let tip = function(total, tip = 0.2) {
   let totalWithTip = total + total * tip;
   console.log('Your total with tip is: $' + totalWithTip);
