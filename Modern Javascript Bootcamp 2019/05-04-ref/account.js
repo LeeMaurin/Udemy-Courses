db23b65213f3216199bc46df9290f42e6d9df1f7
Lee Maurin <leem4421@gmail.com>
2019-05-07 18:20:47 -0500

    Update account.js

--- Modern Javascript Bootcamp 2019/05-04-ref/account.js
+++ Modern Javascript Bootcamp 2019/05-04-ref/account.js
@@ -1,3 +1,9 @@
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node filename.js.
+// Note: you must be in the subdirectory where the JavaScript file is located.
+
+
+
 let myAccount = {
   name: 'Lee',
   expenses: 0,
