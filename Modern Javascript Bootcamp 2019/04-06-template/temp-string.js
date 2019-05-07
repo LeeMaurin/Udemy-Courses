518cfc9f6895d3b501d23d3ec20ea68ef5eae051
Lee Maurin <leem4421@gmail.com>
2019-05-06 19:08:36 -0500

    Update temp-string.js

--- Modern Javascript Bootcamp 2019/04-06-template/temp-string.js
+++ Modern Javascript Bootcamp 2019/04-06-template/temp-string.js
@@ -2,6 +2,8 @@
 // At the terminal prompt, type: node temp-string.js. 
 // Note: you must be in the subdirectory where the JavaScript file is located.
 
+This code utilizes a function to generate a statement.
+
 let tip = function(total, tip = 0.2) {
   let totalWithTip = total + total * tip;
   console.log(`A ${tip * 100}% tip on $${total} would be $${total * tip}`);
