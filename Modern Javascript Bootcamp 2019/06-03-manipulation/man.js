46d0ebedc94606c0a44fca5ba80edff0c9e9ed6a
Lee Maurin <leem4421@gmail.com>
2019-05-09 16:20:28 -0500

    Update man.js

--- Modern Javascript Bootcamp 2019/06-03-manipulation/man.js
+++ Modern Javascript Bootcamp 2019/06-03-manipulation/man.js
@@ -1,3 +1,8 @@
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node filename.js.
+// Note: you must be in the subdirectory where the JavaScript file is located.
+
+
 let todo = ['feed cat', 'workout', 'cook', 'eat', 'sleep'];
 
-console.log(todo.);
+console.log(todo);
