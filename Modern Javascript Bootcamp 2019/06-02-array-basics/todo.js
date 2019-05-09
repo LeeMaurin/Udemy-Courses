9b56afacded309f211257339d8e6ebd2f29974f9
Lee Maurin <leem4421@gmail.com>
2019-05-08 21:55:32 -0500

    Update todo.js

--- Modern Javascript Bootcamp 2019/06-02-array-basics/todo.js
+++ Modern Javascript Bootcamp 2019/06-02-array-basics/todo.js
@@ -1,3 +1,9 @@
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node filename.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
+
+
+
 const toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
 console.log(`You have ${toDoList.length} todos.`);
 console.log(`ToDos: ${toDoList[0]}`);
