a2417d99671d20a6537620e3418fecc09818b0fc
Lee Maurin <leem4421@gmail.com>
2019-05-08 22:06:24 -0500

    Update todo.js

--- Modern Javascript Bootcamp 2019/06-02-array-basics/todo.js
+++ Modern Javascript Bootcamp 2019/06-02-array-basics/todo.js
@@ -2,10 +2,10 @@
 // At the terminal prompt, type: node filename.js. 
 // Note: you must be in the subdirectory where the JavaScript file is located.
 
-// The toDoList array is utilized in three ways:
-// 1. 
+// The toDoList array is utilized to generate three outputs: 
 
 const toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
+
 console.log(`You have ${toDoList.length} todos.`);  // returns "You have 5 todos"
 console.log(`ToDos: ${toDoList[0]}`); // returns the first array element in "ToDos: walk the dog"
 console.log(`ToDos: ${toDoList[toDoList.length - 2]}`); // returns the second-to-last element in "ToDos: eat" 
