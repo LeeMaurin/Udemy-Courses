4f1b9b434df73eb18bf3ebe6c823b13d324fb3d8
Lee Maurin <leem4421@gmail.com>
2019-05-08 21:46:31 -0500

    Update string-methods.js

--- Modern Javascript Bootcamp 2019/05-07-built-in-methods/string-methods.js
+++ Modern Javascript Bootcamp 2019/05-07-built-in-methods/string-methods.js
@@ -1,4 +1,6 @@
-
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node filename.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
 
 let isValidPassword = function(pw) {
   return pw.length > 8 && !pw.includes('password');
