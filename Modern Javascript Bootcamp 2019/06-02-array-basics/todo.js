a4ee571955f260181b9fc6532f7b62da14ed50e2
Lee Maurin <leem4421@gmail.com>
2019-05-08 22:04:24 -0500

    Update todo.js

--- Modern Javascript Bootcamp 2019/06-02-array-basics/todo.js
+++ Modern Javascript Bootcamp 2019/06-02-array-basics/todo.js
@@ -8,4 +8,4 @@
 const toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
 console.log(`You have ${toDoList.length} todos.`);  // returns "You have 5 todos"
 console.log(`ToDos: ${toDoList[0]}`); // returns the first array element in "ToDos: walk the dog"
-console.log(`ToDos: ${toDoList[toDoList.length - 2]}`); 
+console.log(`ToDos: ${toDoList[toDoList.length - 2]}`); // returns the second-to-last element in "ToDos: eat" 
