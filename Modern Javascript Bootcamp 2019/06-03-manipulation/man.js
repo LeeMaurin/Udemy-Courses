1458bd702e1b0f398f0203fde4076497045da9aa
Lee Maurin <leem4421@gmail.com>
2019-05-09 16:21:15 -0500

    Update man.js

--- Modern Javascript Bootcamp 2019/06-03-manipulation/man.js
+++ Modern Javascript Bootcamp 2019/06-03-manipulation/man.js
@@ -1,6 +1,8 @@
 // This is a stand-alone JavaScript file. It can be run in node.js.
 // At the terminal prompt, type: node filename.js.
-// Note: you must be in the subdirectory where the JavaScript file is located.
+// Note: you must be in the subdirectory where the JavaScript file is located.Thi
+
+// This code just prints the contents of the array to the console.
 
 
 let todo = ['feed cat', 'workout', 'cook', 'eat', 'sleep'];
