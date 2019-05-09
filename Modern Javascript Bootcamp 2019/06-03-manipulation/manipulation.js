b358a3a96b2909efc87c518e67abc4bd0a67dad9
Lee Maurin <leem4421@gmail.com>
2019-05-09 16:22:29 -0500

    Update manipulation.js

--- Modern Javascript Bootcamp 2019/06-03-manipulation/manipulation.js
+++ Modern Javascript Bootcamp 2019/06-03-manipulation/manipulation.js
@@ -1,3 +1,10 @@
+// This is a stand-alone JavaScript file. It can be run in node.js.
+// At the terminal prompt, type: node filename.js.
+// Note: you must be in the subdirectory where the JavaScript file is located.
+
+
+
+
 let toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
 
 console.log(toDoList.splice(2, 1));
