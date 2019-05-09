d636c75647c946dcc7998b5e2f5c9fbdc5337c89
Lee Maurin <leem4421@gmail.com>
2019-05-08 21:45:09 -0500

    Update string-methods.js

--- Modern Javascript Bootcamp 2019/05-07-built-in-methods/string-methods.js
+++ Modern Javascript Bootcamp 2019/05-07-built-in-methods/string-methods.js
@@ -1,16 +1,4 @@
-// let isValidPassword = function(pw) {
-//   if (pw.length > 8 && !pw.includes('password')) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
 
-// console.log(isValidPassword('sdfsf'));
-// console.log(isValidPassword('sdsflkdjfl'));
-// console.log(isValidPassword('sdfsdpassword'));
-
-// OR
 
 let isValidPassword = function(pw) {
   return pw.length > 8 && !pw.includes('password');
