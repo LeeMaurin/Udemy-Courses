328ed356cde1ab140d2b74f2b30af95fe2cb4471
Lee Maurin <leem4421@gmail.com>
2019-05-10 21:53:09 -0500

    Update loop.js

--- Modern Javascript Bootcamp 2019/06-04.5-for-loop/loop.js
+++ Modern Javascript Bootcamp 2019/06-04.5-for-loop/loop.js
@@ -1,5 +1,10 @@
-let toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
+// This is a stand-alone JavaScript file. It can be run in node.js. 
+// At the terminal prompt, type: node filename.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
+// 
+// The for loop prints out the elements of the array.
 
+let toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
 
 for (let count = 0; count < toDoList.length -1; count++) {
   console.log(`${count + 1}. ${toDoList[count]} `);
