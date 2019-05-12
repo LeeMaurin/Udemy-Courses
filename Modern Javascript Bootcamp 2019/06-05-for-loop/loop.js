7bc799c3189895b9e6d389acb81333dac55694d8
Lee Maurin <leem4421@gmail.com>
2019-05-11 23:06:06 -0500

    Update loop.js

--- Modern Javascript Bootcamp 2019/06-05-for-loop/loop.js
+++ Modern Javascript Bootcamp 2019/06-05-for-loop/loop.js
@@ -2,6 +2,8 @@
 // At the terminal prompt, type: node filename.js. 
 // Note: you must be in the subdirectory where the JavaScript file is located.
 
+// This for loop writes the array to the console with array index numbers
+
 let toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
 
 for (let count = 0; count < toDoList.length; count++) {
