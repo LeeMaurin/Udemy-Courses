4fdf381f99646bb275ab6d5a15b9a71d3e062544
Lee Maurin <leem4421@gmail.com>
2019-05-11 23:04:22 -0500

    Update loop.js

--- Modern Javascript Bootcamp 2019/06-05-for-loop/loop.js
+++ Modern Javascript Bootcamp 2019/06-05-for-loop/loop.js
@@ -1,5 +1,6 @@
 This is a stand-alone JavaScript file. It can be run in node.js. 
-At the terminal prompt, type: node filename.js. Note: you must be in the subdirectory where the JavaScript file is located.
+At the terminal prompt, type: node filename.js. 
+Note: you must be in the subdirectory where the JavaScript file is located.
 
 let toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
 
