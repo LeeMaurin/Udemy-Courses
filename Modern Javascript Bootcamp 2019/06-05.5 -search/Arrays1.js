311419b613c17c3fb55488ea464a1c51d5bab74d
Lee Maurin <leem4421@gmail.com>
2019-05-12 10:57:09 -0500

    Update Arrays1.js

--- Modern Javascript Bootcamp 2019/06-05.5 -search/Arrays1.js
+++ Modern Javascript Bootcamp 2019/06-05.5 -search/Arrays1.js
@@ -1,3 +1,5 @@
+This is a stand-alone JavaScript file. It can be run in node.js. At the terminal prompt, type: node filename.js. Note: you must be in the subdirectory where the JavaScript file is located.
+
 const notes = [
   {
     title: 'my next trip',
