2594a0736b332f6b4f05aee30621869bf26c6401
Lee Maurin <leem4421@gmail.com>
2019-05-11 23:03:06 -0500

    Update loop.js

--- Modern Javascript Bootcamp 2019/06-05-for-loop/loop.js
+++ Modern Javascript Bootcamp 2019/06-05-for-loop/loop.js
@@ -1,18 +1,5 @@
 let toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
 
-// console.log(toDoList.splice(2, 1));
-// console.log(toDoList);
-// toDoList.push('workout');
-// console.log(toDoList);
-// toDoList.pop();
-// console.log(toDoList);
-// console.log(`You have ${toDoList.length} todos.`);
-
-// toDoList = toDoList.splice(0, 2);
-// toDoList.forEach(function(item, index) {
-//   console.log(`${index + 1}. ${item}. `);
-// });
-
 for (let count = 0; count < toDoList.length; count++) {
   console.log(`${count + 1}. ${toDoList[count]} `);
 }
