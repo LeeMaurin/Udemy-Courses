57dd66b2e375b5480f5d7d53de94152a40666cb6
Lee Maurin <leem4421@gmail.com>
2019-05-11 23:07:08 -0500

    Update loop.js

--- Modern Javascript Bootcamp 2019/06-05-for-loop/loop.js
+++ Modern Javascript Bootcamp 2019/06-05-for-loop/loop.js
@@ -6,6 +6,6 @@
 
 let toDoList = ['walk the dog', 'walk the cat', 'code', 'eat', 'cook'];
 
-for (let count = 0; count < toDoList.length; count++) {
+for (let count = 0; count < toDoList.length-1; count++) {
   console.log(`${count + 1}. ${toDoList[count]} `);
 }
