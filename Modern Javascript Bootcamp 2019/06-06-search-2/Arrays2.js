ea22a9530aaa97129be23bf61b2055a73bc64752
Lee Maurin <leem4421@gmail.com>
2019-05-13 20:26:25 -0500

    Update Arrays2.js

--- Modern Javascript Bootcamp 2019/06-06-search-2/Arrays2.js
+++ Modern Javascript Bootcamp 2019/06-06-search-2/Arrays2.js
@@ -1,5 +1,5 @@
-
-This is a stand-alone JavaScript file. It can be run in node.js. At the terminal prompt, type: node filename.js. Note: you must be in the subdirectory where the JavaScript file is located.
+This is a stand-alone JavaScript file. It can be run in node.js. 
+At the terminal prompt, type: node filename.js. Note: you must be in the subdirectory where the JavaScript file is located.
 
 
 
