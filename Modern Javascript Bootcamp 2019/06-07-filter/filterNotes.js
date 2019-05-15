47592b97c1244a1bfaced18380d58983e3d6d034
Lee Maurin <leem4421@gmail.com>
2019-05-14 21:05:22 -0500

    Update filterNotes.js

--- Modern Javascript Bootcamp 2019/06-07-filter/filterNotes.js
+++ Modern Javascript Bootcamp 2019/06-07-filter/filterNotes.js
@@ -1,6 +1,6 @@
-This is a stand-alone JavaScript file. It can be run in node.js. 
-At the terminal prompt, type: node filterNotes.js. 
-Note: you must be in the subdirectory where the JavaScript file is located.
+// This is a stand-alone JavaScript file. It can be run in node.js. 
+// At the terminal prompt, type: node filterNotes.js. 
+// Note: you must be in the subdirectory where the JavaScript file is located.
 
 
 const notes = [{
