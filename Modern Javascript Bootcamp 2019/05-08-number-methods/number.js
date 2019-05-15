c900d86d9d7f16f9e5ea33ef545ca611ffa5701e
Lee Maurin <leem4421@gmail.com>
2019-05-15 08:16:17 -0500

    Update number.js

--- Modern Javascript Bootcamp 2019/05-08-number-methods/number.js
+++ Modern Javascript Bootcamp 2019/05-08-number-methods/number.js
@@ -1,5 +1,5 @@
 // This is a stand-alone JavaScript file. It can be run in node.js.
-// At the terminal prompt, type: node filename.js.
+// At the terminal prompt, type: node number.js.
 // Note: you must be in the subdirectory where the JavaScript file is located.
 
 // makeGuess compares a known number from 1 to 10 to a randomly generated number from 1 to 10.
