5672748c9af878e0b550cc8ab0f0a5a41f285a63
Lee Maurin <leem4421@gmail.com>
2019-05-16 21:50:20 -0500

    Update and-or.js

--- Modern Javascript Bootcamp 2019/03-09-and-or/and-or.js
+++ Modern Javascript Bootcamp 2019/03-09-and-or/and-or.js
@@ -1,5 +1,5 @@
 // This is a stand-alone JavaScript file. It can be run in node.js. 
-// At the terminal prompt, type: node and-or.js. 
+// At the terminal prompt, type: node and-or.js.  
 // Note: you must be in the subdirectory where the JavaScript file is located.
 
 // This code catagorizes restaurant menus into three types:  vegan, vegan & meat and meat lovers.
