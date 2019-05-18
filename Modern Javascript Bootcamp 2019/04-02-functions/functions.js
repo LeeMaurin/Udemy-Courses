300d2b917f32836912a67ac29588c2fef0025612
Lee Maurin <leem4421@gmail.com>
2019-05-17 23:46:50 -0500

    Update functions.js

--- Modern Javascript Bootcamp 2019/04-02-functions/functions.js
+++ Modern Javascript Bootcamp 2019/04-02-functions/functions.js
@@ -12,5 +12,5 @@ let convert = function(temp) {
 let temp1 = convert(32);
 let temp2 = convert(212);
 
-console.log(temp1 + ' degrees celsius');
+console.log(temp1 + ' degrees celsius'); 
 console.log(temp2 + ' degrees celsius');
