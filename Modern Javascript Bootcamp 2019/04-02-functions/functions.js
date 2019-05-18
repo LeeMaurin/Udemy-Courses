7bc1b1e85280e7235c9515bddcceee79e06a5ea0
Lee Maurin <leem4421@gmail.com>
2019-05-17 23:46:36 -0500

    Update functions.js

--- Modern Javascript Bootcamp 2019/04-02-functions/functions.js
+++ Modern Javascript Bootcamp 2019/04-02-functions/functions.js
@@ -1,6 +1,6 @@
 // This is a stand-alone JavaScript file. It can be run in node.js. 
 // At the terminal prompt, type: node functions.js.  
-// Note: you must be in the subdirectory where the JavaScript file is located.
+// Note: you must be in the subdirectory where the JavaScript file is located. 
 
 // This code utilizes a function to convert a farenheit temperature into celcius.
 
