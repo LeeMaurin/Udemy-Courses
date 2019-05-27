119cd363aba0b8ed84c50209dd37b268d7cc2e45
Lee Maurin <leem4421@gmail.com>
2019-05-27 11:29:46 -0500

    Add files via upload

---
+++ The Modern React Bootcamp/09.   Embedding JavaScript in JSX/index.js
@@ -0,0 +1,16 @@
+function myMood() {
+  const moods = ['sleepy', 'happy', 'angry', 'silly', 'quiet'];
+  return moods[Math.floor(Math.random() * moods.length)];
+}
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>My current mood is: {myMood()}</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
