71c36be51469b6240355ec233a879bca6f8f6bb1
Lee Maurin <leem4421@gmail.com>
2019-06-26 16:41:52 -0500

    Add files via upload

---
+++ The Complete JavaScript Course 2019/009. Variable Mutation and Type Coersion/script.js
@@ -0,0 +1,21 @@
+var firstName = 'John';
+console.log(firstName);
+
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+console.log(
+  firstName + ' is a ' + age + 'year old. Is he married? ' + isMarried
+);
+
+job = 'Coach';
+age = 'Twenty Eight';
+
+alert(firstName + ' is a ' + age + 'year old. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
