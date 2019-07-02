008210986eeed7e761ae7bbba27c15a8426a2100
Lee Maurin <leem4421@gmail.com>
2019-07-02 12:33:27 -0500

    Add files via upload

---
+++ The Complete JavaScript Course 2019/011. Operator Precedence/script.js
@@ -0,0 +1,15 @@
+// Operator Precedence
+
+var markWeight = 82; //kilograms
+var markHeight = 1.59; //meters
+
+var johnWeight = 89;
+var johnHeight = 1.62;
+
+var markBMI = markWeight / (markHeight * markHeight);
+var johnBMI = johnWeight / (johnHeight * johnHeight);
+console.log(markBMI, johnBMI);
+
+var markHigherBMI = markBMI > johnBMI;
+
+console.log("Is Mark's BMI higher than John's BMI: " + markHigherBMI);
