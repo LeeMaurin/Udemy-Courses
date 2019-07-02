23dcaecf62c993c13eb2d0038f2e9421c2095b1e
Lee Maurin <leem4421@gmail.com>
2019-07-02 12:33:00 -0500

    Add files via upload

---
+++ The Complete JavaScript Course 2019/010. Basic Operators/script.js
@@ -0,0 +1,25 @@
+// Basic Operators
+
+var year, yearJohn, yearMark;
+var now = 2019;
+var ageJohn = 26;
+var ageMark = 33;
+
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+console.log(yearMark);
+
+// Logical Operators
+
+var johnOlder = yearJohn < yearMark;
+console.log(johnOlder);
+
+// typeof Operator
+
+console.log(typeof johnOlder);
+console.log(typeof yearMark);
+console.log(typeof 'Something Here');
+var x;
+console.log(typeof x);
